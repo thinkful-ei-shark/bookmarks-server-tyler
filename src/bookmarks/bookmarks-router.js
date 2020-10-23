8e28276cfceabe559c5b0c84ebe3c5d8acbd5bb4
Tyler Sharpee <sharpee94@gmail.com>
2020-10-23 16:27:19 -0400

    add data store

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -4,25 +4,8 @@ const {v4: uuid} = require('uuid');
 const logger = require('../logger');
 
 // Local Dependencies
-const {PORT} = require('./config');
-
-// Placeholder Variables
-const bookmarks = [
-  {
-    'id': '8sdfbvbs65sd',
-    'title': 'Google',
-    'url': 'http://google.com',
-    'desc': 'An indie search engine startup',
-    'rating': 4
-  },
-  {
-    'id': '87fn36vd9djd',
-    'title': 'Fluffiest Cats in the World',
-    'url': 'http://medium.com/bloggerx/fluffiest-cats-334',
-    'desc': 'The only list of fluffy cats online',
-    'rating': 5
-  }
-];
+const {PORT} = require('../config');
+const bookmarks = require('../store');
 
 // Middleware
 const bookmarksRouter = express.Router();
