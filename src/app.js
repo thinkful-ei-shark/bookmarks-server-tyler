b0189bd502334639b7991083576fee37afc437e3
Tyler Sharpee <sharpee94@gmail.com>
2020-10-23 13:17:35 -0400

    implement authorization

--- src/app.js
+++ src/app.js
@@ -11,10 +11,27 @@ const winston = require('winston');
 const {NODE_ENV} = require('./config');
 const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+// Placeholder Variables
+const bookmarks = [
+  {
+    'id': '8sdfbvbs65sd',
+    'title': 'Google',
+    'url': 'http://google.com',
+    'desc': 'An indie search engine startup',
+    'rating': 4
+  },
+  {
+    'id': '87fn36vd9djd',
+    'title': 'Fluffiest Cats in the World',
+    'url': 'http://medium.com/bloggerx/fluffiest-cats-334',
+    'desc': 'The only list of fluffy cats online',
+    'rating': 5
+  }
+];
 
+const morganOption = (NODE_ENV === 'production')? 'tiny': 'common';
+
+// Setup Winston Logger
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
@@ -35,6 +52,19 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+// Authorization Middleware
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  console.log('apiToken', apiToken);
+  console.log('authToken',authToken);
+  if (!authToken || authToken.split(' ')[0] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  // move to the next middleware
+  next();
+});
 
 // Routes
 app.get('/', (req,res) => {
